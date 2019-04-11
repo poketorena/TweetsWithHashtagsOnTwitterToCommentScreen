9e6c6e23cb92e6b44bc51ec6e621ecfc86b15414
poketorena <poketorena@users.noreply.github.com>
2019-04-11 21:31:53 +0900

    とりあえず完成
    ハッシュタグの除去は不完全なので後で修正すること。
    フィルターストリームなので鍵アカウントは未対応。フォローしているユーザーすべてをID指定すればできるかもしれない。

--- TweetsWithHashtagsOnTwitterToCommentScreen/Program.cs
+++ TweetsWithHashtagsOnTwitterToCommentScreen/Program.cs
@@ -1,7 +1,9 @@
-﻿using CoreTweet;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CoreTweet;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Threading;
+using OpenQA.Selenium.Chrome;
 
 namespace TweetsWithHashtagsOnTwitterToCommentScreen
 {
@@ -17,9 +19,11 @@ namespace TweetsWithHashtagsOnTwitterToCommentScreen
                 "AccessTokenSecret"
                 );
 
+            var hashtagText = "ctrl_sintyoku";
+
             // フィルターストリームへの接続とツイート時の対応
             var disposable = tokens.Streaming
-                .FilterAsObservable(track => "#ctrl_sintyoku")
+                .FilterAsObservable(track => $"#{hashtagText}")
                 .Subscribe(streamingMessage =>
                 {
                     // JSONをパース
@@ -34,12 +38,51 @@ namespace TweetsWithHashtagsOnTwitterToCommentScreen
                     // 改行
                     Console.WriteLine();
 
-                    // 「#ctrl_sintyoku」を除去したツイート本文
-                    Console.WriteLine(text.Replace("#ctrl_sintyoku", ""));
+                    // ツイート本文からハッシュタグのテキストを除去する（デフォルトでは「#ctrl_sintyoku」を除去する）
+                    Console.WriteLine(text.Replace($"#{hashtagText}", ""));
+
+                    var hashtagRemovedText = text.Replace($"#{hashtagText}", "");
+
+                    // ChromeOptionsオブジェクトを生成する
+                    var options = new ChromeOptions();
+
+                    // --headlessを追加（これを追加するとバックグラウンドでChromeを起動できる）
+                    options.AddArgument("--headless");
+
+                    // ChromeOptions付きでChromeDriverオブジェクトを生成する
+                    var chrome = new ChromeDriver(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), options);
+
+                    // URLに移動する
+                    chrome.Url = @"https://commentscreen.com/comments?room=" + hashtagText;
+
+                    // テキストを自動入力する
+                    chrome.FindElementByClassName("type_msg").SendKeys(hashtagRemovedText);
+
+                    // 送信ボタンを押す
+                    chrome.FindElementByClassName("send_btn").Click();
+
+                    // ブラウザを閉じる
+                    chrome.Quit();
+
+                    // 送信確認用コメント
+                    Console.WriteLine($"CommentScreenに「{hashtagRemovedText}」を送信しました");
                 });
 
-            // 疑似的な無限待機
-            Thread.Sleep(int.MaxValue);
+            // 接続確認用コメント
+            Console.WriteLine("フィルターストリームに接続しました");
+
+            // 以下終了のための処理
+            while (true)
+            {
+                // キー入力をチェックし、Eが入力されたらプログラムを終了する
+                if (Console.KeyAvailable)
+                {
+                    if (Console.ReadKey().Key.ToString() == "E")
+                    {
+                        return;
+                    }
+                }
+            }
         }
     }
 }
